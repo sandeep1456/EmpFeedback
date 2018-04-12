1baf0f4fca2486028e0d5c8de1856341782c2800
sandeep1456 <kamblesandeepr@gmail.com>
2018-04-12 16:35:32 +0530

    React App's basic code structure #1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends Component {
+render(){
+  return <div> Emplyee Feedback App </div>
+}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
